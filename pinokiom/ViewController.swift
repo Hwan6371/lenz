419feacbd42a9c211135ff46ca6f6a65c7146e40
Hwan6371 <lenz6371@gmail.com>
2020-05-03 15:19:10 +0900

    Initial Commit

---
+++ pinokiom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pinokiom
+//
+//  Created by 김재환 on 2020/05/03.
+//  Copyright © 2020 김재환. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
